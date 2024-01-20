3eea8360769957d941c5dd3f03c24a98234ef993
ishween <ishweensehmbhi@gmail.com>
2024-01-20 18:55:48 -0500

    complete exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,30 +4,26 @@ import Total from "./Total";
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = {
-		name: "Fundamentals of React",
-		exercises: 10,
-	};
-	const part2 = {
-		name: "Using props to pass data",
-		exercises: 7,
-	};
-	const part3 = {
-		name: "State of a component",
-		exercises: 14,
-	};
+	const parts = [
+		{
+			name: "Fundamentals of React",
+			exercises: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercises: 7,
+		},
+		{
+			name: "State of a component",
+			exercises: 14,
+		},
+	];
 
 	return (
 		<>
 			<Header courseName={course}></Header>
-			<Content courseList={[part1, part2, part3]}></Content>
-			<Total
-				itemsList={[
-					part1.exercises,
-					part2.exercises,
-					part3.exercises,
-				]}
-			></Total>
+			<Content courseList={parts}></Content>
+			<Total itemsList={parts}></Total>
 		</>
 	);
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -2,7 +2,9 @@ const Total = (props) => {
 	return (
 		<p>
 			Number of exercises{" "}
-			{props.itemsList[0] + props.itemsList[1] + props.itemsList[2]}
+			{props.itemsList[0].exercises +
+				props.itemsList[1].exercises +
+				props.itemsList[2].exercises}
 		</p>
 	);
 };
